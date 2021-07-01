a8ab1a9c8514db51c16d516913f3f76b4c57eb61
Tosja <82406129+Tosja@users.noreply.github.com>
2021-07-01 14:09:18 +0300

    file
    file

---
+++ header.js
@@ -0,0 +1,32 @@
+let div;
+let div2;
+let h;
+window.onload = function () {
+    document.getElementById("slide1").onclick = first;
+    document.getElementById("slide2").onclick = second;
+    document.getElementById("slide3").onclick = fhree;
+    div = document.getElementById("slider");
+    div2 = document.getElementById("slidbox");
+    h = document.getElementById("slidh");
+    div.className = "slider1";
+    div2.className = "slidbox1";
+    h.innerHTML = "ITALIAN CUIZINE";
+}
+
+function first() {
+    div.className = "slider1";
+    div2.className = "slidbox1";
+    h.innerHTML = "ITALIAN CUIZINE";
+}
+
+function second() {
+    div.className = "slider2";
+    div2.className = "slidbox2";
+    h.innerHTML = "ITALIAN PIZZA";
+}
+
+function fhree() {
+    div.className = "slider3";
+    div2.className = "slidbox3";
+    h.innerHTML = "WE COOKED YOUR DESIRED </br> PIZZA RECIPE";
+}
